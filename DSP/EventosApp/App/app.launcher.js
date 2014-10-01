9f12ed99e4bd820052a4a10cabebb3c64834b628
William S Rodriguez <williamsrz@outlook.com>
2014-10-01 04:58:00 -0300

    opa.. faltou para completar url's limpas \0/

---
+++ DSP/EventosApp/App/app.launcher.js
@@ -0,0 +1,10 @@
+﻿(function () {
+
+    // configuração do modulo para app
+    var app = angular.module("eventosApp", [
+        // dependencias
+        'ngResource', // rotas
+        'ngCookies' // cookies
+    ]);
+
+}());
