ecb427c07433e9d30aefd26f1f36785fe7e62803
William S Rodriguez <williamsrz@outlook.com>
2014-10-01 14:48:31 -0300

    layout, layout, layout...

--- DSP/EventosApp/App/Services/eventosService.js
+++ DSP/EventosApp/App/Services/eventosService.js
@@ -23,7 +23,9 @@
                     inicio: '29/08/2014',
                     termino: '30/09/2014',
                     organizador: 'Lambda3',
-                    local: 'Microsoft São Paulo'
+                    local: 'Microsoft São Paulo',
+                    banner: 'Images/eventos/almsummit2014.png',
+                    descricao: 'Realizado anualmente desde 2011, o ALM Summit Brasil é o principal evento do país.'
                 },
                 {
                     id: '2',
@@ -31,7 +33,9 @@
                     inicio: '16/10/2014',
                     termino: '18/10/2014',
                     organizador: 'Global Code',
-                    local: 'Porto Alegre'
+                    local: 'Porto Alegre',
+                    banner: 'Images/eventos/tdc.png',
+                    descricao: 'Realizado anualmente desde 2011, o ALM Summit Brasil é o principal evento do país.'
                 },
                 {
                     id: '3',
@@ -39,7 +43,20 @@
                     inicio: '23/11/2014',
                     termino: '10/12/2014',
                     organizador: 'Arquitetura',
-                    local: 'Jundiai'
+                    local: 'Jundiai',
+                    banner: 'Images/eventos/treinamento-java.png',
+                    descricao: 'Realizado anualmente desde 2011, o ALM Summit Brasil é o principal evento do país.'
+                }
+                ,
+                {
+                    id: '4',
+                    titulo: 'Workshop SharePoint ALM',
+                    inicio: '11/12/2014',
+                    termino: '12/12/2014',
+                    organizador: '#DescubraSharePoint',
+                    local: 'Curitiba',
+                    banner: 'Images/eventos/sharepointalm.png',
+                    descricao: 'Realizado anualmente desde 2011, o ALM Summit Brasil é o principal evento do país.'
                 }
         ];
 
